34c946051c62033c8a74519dd6f9bd5c4974f545
Vieraaaas <144917497+Vieraaaas@users.noreply.github.com>
2024-01-11 14:21:08 +0100

    Solve exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argument) {
+  const result = [];
+  array.forEach((item) => {
+    if(!argument.includes(item)) {
+      result.push(item);
+    }
+  })
+  return result;
 
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
