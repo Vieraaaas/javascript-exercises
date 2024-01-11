74d099241f9e58cfc055cc32b2957df5262424e9
Vieraaaas <144917497+Vieraaaas@users.noreply.github.com>
2024-01-11 15:29:45 +0100

    Solve exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(par1, par2) {
+  if (par1 >= 0 && 
+      par2 >= 0 && 
+      Number.isInteger(par1) && 
+      Number.isInteger(par2)) {
+        if (par1 < par2) {
+          let result = par1;
+          let n = par1;
+          for (let i = 0; i < (par2 - par1); i++) {
+            n += 1;
+            result += n;
+          }
+          return result;
 
+      } else if (par1 > par2) {
+          let result = par2;
+          let n = par2;
+          for (let i = 0; i < (par1 - par2); i++) {
+            n += 1;
+            result += n;
+          }
+          return result;
+        } 
+    } else {
+      return "ERROR"
+  }
 };
 
 // Do not edit below this line
